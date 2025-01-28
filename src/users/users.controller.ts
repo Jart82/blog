74262b911e3221fb843840486e6d4af0e7167882
Jeremiah Agarins <jagarin@seamfix.com>
2025-01-28 14:13:01 +0100

    create user get and post endpoints with params, body and query

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Body } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params);
+    console.log(query);
+    return 'You sent a get request to user endpoint';
+  }
+
+  @Post()
+  public createUsers(@Body() request: any) {
+    console.log(request);
+    return 'You sent a post request to user endpoint';
+  }
+}
