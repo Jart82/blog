09c98598519f775703c4ccfc26e61aaa70a4b6d2
Jeremiah Agarins <jagarin@seamfix.com>
2025-01-30 10:17:10 +0100

    adding pipes to endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Param, Post, Query, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Body, Headers, Ip, ParseIntPipe } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(@Param('id', ParseIntPipe) id: number | undefined, @Query('limit', ParseIntPipe) limit: number) {
+    console.log(typeof id);
+    console.log(typeof limit);
     return 'You sent a get request to user endpoint';
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
-    console.log(request);
+  public createUsers(
+    @Body('email') email: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(email);
+    console.log(headers);
+    console.log(ip);
     return 'You sent a post request to user endpoint';
   }
 }
